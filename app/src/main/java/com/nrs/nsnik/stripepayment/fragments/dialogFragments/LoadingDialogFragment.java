ae5be08b49dbefce202a873606780873af225239
nsnikhil <n.nikhil.ns65@gmail.com>
2017-05-03 21:52:04 +0530

    test

--- app/src/main/java/com/nrs/nsnik/stripepayment/fragments/dialogFragments/LoadingDialogFragment.java
+++ app/src/main/java/com/nrs/nsnik/stripepayment/fragments/dialogFragments/LoadingDialogFragment.java
@@ -10,21 +10,16 @@ import android.view.ViewGroup;
 
 import com.nrs.nsnik.stripepayment.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class LoadingDialogFragment extends DialogFragment {
 
 
     public LoadingDialogFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_loading_dialog, container, false);
     }
 
